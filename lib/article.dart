2b8d1a1260829e15845f9e7b3efbfbf470214528
rosemei1 <hanifirfan414@gmail.com>
2023-03-27 12:52:20 +0700

    ganti

--- lib/article.dart
+++ lib/article.dart
@@ -1,43 +1,55 @@
 import 'package:flutter/material.dart';
-
+import 'dart:ui';
 
 class Artikel extends StatelessWidget {
   const Artikel ({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          'Card Title',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        const Divider(
+          color: Colors.grey,
+          height: 20,
+          thickness: 1,
+          indent: 20,
+          endIndent: 0,
         ),
-        Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const ListTile(
-                title: Text('The Enchanted Nightingale'),
-                subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  TextButton(
-                    child: const Text('BUY TICKETS'),
-                    onPressed: () {/* ... */},
-                  ),
-                  const SizedBox(width: 8),
-                  TextButton(
-                    child: const Text('LISTEN'),
-                    onPressed: () {/* ... */},
-                  ),
-                  const SizedBox(width: 8),
-                ],
-              ),
-            ],
+        Container(
+          margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 2.0, bottom: 2.0),
+          child: Text(
+            'Bacaan Terbaru',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color:Color.fromARGB(255, 154, 191, 21)),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 1.0),
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  title: Text('The Enchanted Nightingale'),
+                  subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('BUY TICKETS'),
+                      onPressed: () {/* ... */},
+                    ),
+                    const SizedBox(width: 8),
+                    TextButton(
+                      child: const Text('LISTEN'),
+                      onPressed: () {/* ... */},
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ],
